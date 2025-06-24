4456a7f74921c6f25204282546b9e28901424919
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-06-25 04:05:01 +0800

    Add function for correct input validation

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,10 @@ function updateOperand(newOperand){
     operand = newOperand;
 }
 
+function isValidOperand(operand){
+    const isInteger = /(^0$)|(^-?[1-9]+\d*$)/;
+    const isDecimal = /(^-?0\.\d*[1-9]+$)|(^-?[1-9]+\d*\.\d*[1-9]+$)/;
+    return isInteger.test(operand) || isDecimal.test(operand);
+}
+
 window.addEventListener("load",updateOperand(DEFAULT_OPERAND));
